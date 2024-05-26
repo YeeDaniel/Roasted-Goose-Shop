6f7f6404984dc992bbef9fe6a46d3651f61ab590
黃鉅燊 <matchaskill@gmail.com>
2024-05-26 19:21:21 +0800

    add: remove google

--- routes/__init__.py
+++ routes/__init__.py
@@ -1,3 +1,2 @@
 from .default import default
 from .user import user
-from .google import google
--- routes/user.py
+++ routes/user.py
@@ -3,33 +3,31 @@ from services import UserService
 
 user = Blueprint('user', __name__, template_folder='templates')
 
-@user.route("/register", methods=["GET", "POST"])
+@user.route("/register", methods=["POST"])
 def register():
-    if request.method == "POST":
-        username = request.form["username"]
-        email = request.form["email"]
-        password = request.form["password"]
+    username = request.form["username"]
+    email = request.form["email"]
+    password = request.form["password"]
 
-        result = UserService.register(username, email, password)
-        if result:
-            flash("註冊成功", "register")
-            return redirect(url_for("user.login"))
-        else:
-            flash("E-mail 已註冊，請使用登入", "register")
+    result = UserService.register(username, email, password)
+    if result:
+        flash("註冊成功", "register")
+        return redirect(url_for("user.login"))
+    else:
+        flash("E-mail 已註冊，請使用登入", "register")
 
     return render_template("index.html")
 
-@user.route("/login", methods=["GET", "POST"])
+@user.route("/login", methods=["POST"])
 def login():
-    if request.method == "POST":
-        email = request.form["email"]
-        password = request.form["password"]
+    email = request.form["email"]
+    password = request.form["password"]
 
-        login_user = UserService.login(email, password)
-        if login_user:
-            flash("登入成功", "login")
-            return redirect(url_for("index.index"))
-        else:
-            flash("登入失敗", "login")
+    login_user = UserService.login(email, password)
+    if login_user:
+        flash("登入成功", "login")
+        return redirect(url_for("index.index"))
+    else:
+        flash("登入失敗", "login")
 
     return render_template("index.html")
