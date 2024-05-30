d735af1e88f207e5b2b64162382654f4732252fb
YeeDaniel <dgjykfm308@gmail.com>
2024-05-30 19:03:39 +0800

    delete: route

--- routes/__init__.py
+++
@@ -1,7 +0,0 @@
-from .default import default
-from .user import user
-from .stock import stock
-from .product_stock import product_stock
-from .product import product
-from .order import order
-from .product_order import product_order
